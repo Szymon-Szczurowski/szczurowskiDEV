e260386d38a50a2d443a5ea056e7d28393713006
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2022-12-11 12:22:37 +0100

    contact, footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-particlesJS();
+
+const footerYear = document.querySelector('.footer__year')
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear();
+    footerYear.innerText = year;
+}
+
+handleCurrentYear();
+
+// particlesJS();
