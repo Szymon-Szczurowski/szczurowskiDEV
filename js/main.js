b3ebb626d20c2ba6a1524a1482fe1364f66ac2b0
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2022-12-17 19:43:18 +0100

    admendments;

--- js/main.js
+++ js/main.js
@@ -1,11 +1,42 @@
-
 const footerYear = document.querySelector('.footer__year')
 
+const heading = document.querySelector('.about__heading')
+const aboutBox = document.querySelector('.about__box')
+const contactBox = document.querySelector('.contact__box')
+
+const loader = document.getElementById("preloader");
+
 const handleCurrentYear = () => {
-    const year = new Date().getFullYear();
-    footerYear.innerText = year;
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
 }
 
 
-handleCurrentYear();
+document.addEventListener('scroll', function () {
+  // Sprawdź, czy użytkownik jest na sekcji .about
+  const aboutPosition = document.querySelector('.about').getBoundingClientRect().top
+  const windowHeight = window.innerHeight
+
+  if (aboutPosition < windowHeight) {
+    // Jeśli tak, dodaj klasy do elementów
+    heading.classList.add('show')
+    aboutBox.classList.add('show-about')
+  }
+
+  // Sprawdź, czy użytkownik jest na sekcji .contact
+  const contactPosition = document.querySelector('.contact').getBoundingClientRect().top
+
+  if (contactPosition < windowHeight) {
+    // Jeśli tak, dodaj klasę do elementu
+    contactBox.classList.add('show')
+  }
+})
+
+
+window.addEventListener("load", function(){
+  loader.style.display = "none";
+})
+
+
+handleCurrentYear()
 
