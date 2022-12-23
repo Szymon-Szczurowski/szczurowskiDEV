1f3563fb74a58f765d2ca57a42a107ef8bc39b3d
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2022-12-23 22:15:40 +0100

    create burger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 const footerYear = document.querySelector('.footer__year');
 const loader = document.getElementById("preloader");
 
+const burger = document.querySelector(".nav__burger");
+const menu = document.querySelector(".nav__menu");
+const items = document.querySelectorAll(".nav__menu-item");
+
+
+
+const handleNav = () => {
+  menu.classList.toggle('nav__menu--active')
+
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.remove('nav__menu--active')
+    })
+})
+
+
+}
+
+
+
+
 
 // -------------------
 // aktualna data
@@ -25,4 +46,4 @@ const windowLoading = () =>{
 
 windowLoading()
 handleCurrentYear()
-
+burger.addEventListener('click', handleNav)
