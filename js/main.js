1d0d46140328905101ada763bdc55d44ade52b12
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2022-12-27 15:58:26 +0100

    add cross for burger

--- js/main.js
+++ js/main.js
@@ -4,15 +4,18 @@ const loader = document.getElementById("preloader");
 const burger = document.querySelector(".nav__burger");
 const menu = document.querySelector(".nav__menu");
 const items = document.querySelectorAll(".nav__menu-item");
+const cross = document.querySelector(".nav__burger-box");
 
 
 
 const handleNav = () => {
   menu.classList.toggle('nav__menu--active')
+  cross.classList.toggle('nav__burger-cross')
 
   items.forEach(item => {
     item.addEventListener('click', () => {
       menu.classList.remove('nav__menu--active')
+      cross.classList.remove('nav__burger-cross')
     })
 })
 
